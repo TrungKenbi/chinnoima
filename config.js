75186b2ce50b91a553f9ebecc605f7ec2cf1c20e
TrungKenbi <trungkenbi@hotmail.com>
2021-07-07 02:07:43 +0700

    Init ok babe

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Anh biết là đôi khi em cảm thấy buồn vì anh chỉ biết dùng lời nói, nhưng em à, thật lòng anh cũng không muốn vậy vì dịch bệnh mà anh không được ở gần bên em, mong em hiểu và thông cảm cho anh nha!`,
+    btnIntro: 'Okê luôn 👌',
+    title: 'Đừng giận anh nữa nha? 😙',
+    desc: 'Trang web này như một món quà nho nhỏ, cũng như là lời xin lỗi gửi tới em.',
+    btnYes: 'Okee yêu anh <3',
+    btnNo: 'Không, anh ơi :3',
+    question: 'Cảm ơn em vì đã luôn ở bên anh và tin tưởng anh ❤',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chúc em ngủ ngon thiệt là ngon nà!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/I.Am.TrungKenbi/'
+}
